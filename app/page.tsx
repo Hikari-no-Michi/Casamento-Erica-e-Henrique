bf4185a4e2cb7a5ea02dd0f92ee3ec1535b54783
Luiz Henrique <69431619+Hikari-no-Michi@users.noreply.github.com>
2025-03-23 19:55:10 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,9 @@ export default function WeddingReception() {
               onChange={(e) => setName(e.target.value)}
               required
             />
-            <label className="block mt-4 mb-2 text-gray-700">Quantos membros da sua família irão acompanhá-lo?</label>
+            <label className="block mt-4 mb-2 text-gray-700">
+            Confirme quantos membros da sua família virão com você!
+            </label>
             <input
               type="number"
               min="0"
